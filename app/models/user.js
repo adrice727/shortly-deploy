0bfc0b496e031716e516961787a85c895179c817
Hack Reactor Students <accounts@hackreactor.com>
2014-05-14 22:11:21 -0700

    Still trying to sort out issue with mongoose user model methods

--- app/models/user.js
+++ app/models/user.js
@@ -9,37 +9,38 @@ var userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.method('comparePassword', function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-    callback(isMatch);
-  });
-});
-
-userSchema.method('hashPassword', function() {
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.get('password'), null, null).bind(this)
-    .then(function(hash) {
-      this.set('password', hash);
-    });
-});
-
-
-// userSchema.method.comparePassword =  function(attemptedPassword, callback) {
+// userSchema.method('comparePassword', function(attemptedPassword, callback) {
 //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
 //     callback(isMatch);
 //   });
-// };
+// });
 
-// userSchema.method.hashPassword = function() {
+// userSchema.method('hashPassword', function() {
 //   var cipher = Promise.promisify(bcrypt.hash);
 //   return cipher(this.get('password'), null, null).bind(this)
 //     .then(function(hash) {
 //       this.set('password', hash);
 //     });
-// };
+// });
 
 
-var User = db.model('User', userSchema);
+userSchema.method.comparePassword =  function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+userSchema.method.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+};
+
+
+User = db.model('User', userSchema);
+
 
 userSchema.on('init', function(user) {
   user.hashPassword();
